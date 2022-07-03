17bcfbfba4f220a19bc3749c11263a8fbfcc6497
aetherxt <adatanium@gmail.com>
2022-07-03 22:12:10 +0800

    language support

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 app.secret_key = "test"
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template("home.html")
+@app.route("/en")
+@app.route("/home/eng")
+def home_eng():
+    return render_template("home_eng.html")
+
+@app.route("/chi")
+@app.route("/home/chi")
+def home_chi():
+    return render_template("home_chi.html")
